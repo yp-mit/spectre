9299c914c328ab45e8b88d3e3e63056eaadcd90f
Giuseppe Durisi <durisi@gmail.com>
2014-12-20 19:47:16 +0100

    manual: updated outage capacity part

--- rayleigh-block-fading-no-csi/outage_2x2_telatar.m
+++ rayleigh-block-fading-no-csi/outage_2x2_telatar.m
@@ -1,8 +1,7 @@
-
-function R=outage_2x2_telatar(tfdiv,prec,epsilon,snrdb)
+function R=outage_2x2_telatar(L,prec,epsilon,snrdb)
   %
   % optimize outage mi over all input covariance matrices of the form given in [3, eq. ()] (generalized Telatar conjecture)
-  % tfdiv: number of time-frequency diversity branches
+  % L: number of time-frequency diversity branches
   % prec: log2 of the number of samples used in the Monte-Carlo step
   % epsilon: outage probability
   % snrdb: snr in db.
@@ -10,20 +9,20 @@ function R=outage_2x2_telatar(tfdiv,prec,epsilon,snrdb)
 mt=2;
 mr=2;
 
-numIt=tfdiv+1;
-pow_all=zeros(1,tfdiv);
+numIt=L+1;
+pow_all=zeros(1,L);
 R=zeros(numIt,1);
 
-for ii=[numIt],
+for ii=1:1:numIt,
     numIt-ii;
-    bstring=dec2bin(2^(ii-1)-1,tfdiv);
+    bstring=dec2bin(2^(ii-1)-1,L);
     
-    for k=1:1:tfdiv,
+    for k=1:1:L,
         pow_all(k)=str2num(bstring(k))*0.5;        
     end
     
     tic
-    R(ii)=outage_mi(snrdb,epsilon,mt,mr,tfdiv,pow_all,prec)
+    R(ii)=outage_mi(snrdb,epsilon,mt,mr,L,pow_all,prec)
     elapsed_time=toc/60;
     msg=sprintf('time needed to generate samples: %f min', elapsed_time);
     disp(msg); 

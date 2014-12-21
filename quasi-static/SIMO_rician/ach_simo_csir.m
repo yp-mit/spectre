6e793f16768d7ed191b5a17d9ccb3e66bac1d9df
Wei Yang <ywei@chalmers.se>
2014-12-21 15:16:12 +0100

    Increased numerical precision

--- quasi-static/SIMO_rician/ach_simo_csir.m
+++ quasi-static/SIMO_rician/ach_simo_csir.m
@@ -17,6 +17,7 @@ if (nargin < 5) || isempty(K)
 end
 
 loop =  min(1000/error, 10^7);
+loop =  max(loop, 1^5);
 
 if rx ==1
     G= abs(sqrt(K/(K+1))+sqrt(1/(K+1)/2)*(randn(rx,loop)+1i*randn(rx,loop))).^2 ;

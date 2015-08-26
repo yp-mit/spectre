b7cdcbd3efe8fede475de1b25009a2829e323471
austindcollins <austindcollins@localhost.localdomain>
2015-08-25 23:38:46 -0400

    Renamed plotter function to ``plot_all()'' to be consistent, made it take arguements (P,n_t,n_r,T,epsilon), and updated documentation to include this function.

--- csir_fading/plot_all.m
+++ csir_fading/plot_all.m
@@ -1,17 +1,17 @@
-function MIMO_CSIR_plotter
+function plot_all(P,n_t,n_r,T,epsilon)
 
 % Parameters
-vals = []; % vector 
-n_vec = [20:20:800]; % Block lengths to plot
-n_vec = [20 40 80 100 120 160 200 250 300 350 400 ];
-P = 10;              % Power
-n_t = 2;             % # transmit antennas
-n_r = 2;             % # receiver antennas
-T = 2;               % Coherence time
-epsilon = .001;      % (Average) Probability of error
-real_or_complex = 'real'; % Specifies real or complex MIMO channel, set to 'real' for real, 'complex' for complex.
+%n_vec = [20:20:800];      % Block lengths to plot
+n_vec = [20 40 80 120 160 200 250 300 350 400 500 600];
+%P = 10;                   % SNR (in DB)
+%n_t = 2;                  % # transmit antennas
+%n_r = 2;                  % # receiver antennas
+%T = 2;                    % Coherence time
+%epsilon = .001;           % (Average) Probability of error
+real_or_complex = 'real';  % Specifies real or complex MIMO channel, set to 'real' for real, 'complex' for complex.
 
 % Plotting
+vals = [];
 for n = n_vec
     log_M = MIMO_achievability_CSIR(n,epsilon,n_t,n_r,T,P,real_or_complex);   
     vals = [vals 1/(n*T)*log_M]; % Note this is logm for n*T samples, not increasing in 1

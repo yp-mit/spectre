25c21e0519627f0c33c34a5028a854ae77583b9b
Yury Polyanskiy <tcp@tetra.mit.edu>
2016-12-30 14:24:29 -0500

    awgn: converse.m typo

--- awgn/converse.m
+++ awgn/converse.m
@@ -2,5 +2,5 @@ function Lms = converse(Ns, epsil, P)
 % Compute meta-converse upper bound on \log M^*(n, \epsilon, P)
 Lms = [];
 for n = Ns;
-	Lms = [Lms -betaq_low_v2(1-epsil, n+1, P);
+	Lms = [Lms -betaq_low_v2(1-epsil, n+1, P)];
 end

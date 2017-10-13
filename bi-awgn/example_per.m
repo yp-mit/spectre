c1eb8c8fa8db2b8fd5b7053de17acf33e094cce6
erseghe <erseghe@dei.unipd.it>
2017-10-13 16:07:50 +0200

    Fixes for recent Matlab versions (tested with Matlab 2017a)

--- bi-awgn/example_per.m
+++ bi-awgn/example_per.m
@@ -1,18 +1,18 @@
 % PACKET ERROR RATE BOUNDS - LOWER BOUNDS ON ERROR RATE
 
-if(0) % set to (1) if you wish to run the converse_mc function
+if(1) % set to (1) if you wish to run the converse_mc function
 
     n = 1e3;
     R = 1/2;
     Pev = 10.^([-0.5:-0.25:-1.75,-2:-0.5:-7]);
     uno = ones(size(Pev));
 
-    % normal approximation - expected execution time 15 sec
-    SNRdBNA = converse_mc(n*uno,Pev,R*uno,'normal','error');toc; tic
-    % O(n^-2) approximation - expected execution time 120 sec
-    SNRdBPPV = converse_mc(n*uno,Pev,R*uno,'On2','error');toc; tic
-    % O(n^-3) approximation - expected execution time 280 sec
-    SNRdBPPVb = converse_mc(n*uno,Pev,R*uno,'On3','error');toc; tic
+    % normal approximation - expected execution time 4 sec
+    tic; SNRdBNA = converse_mc(n*uno,Pev,R*uno,'normal','error'); toc
+    % O(n^-2) approximation - expected execution time 40 sec
+    tic; SNRdBPPV = converse_mc(n*uno,Pev,R*uno,'On2','error'); toc
+    % O(n^-3) approximation - expected execution time 80 sec
+    tic; SNRdBPPVb = converse_mc(n*uno,Pev,R*uno,'On3','error'); toc
 
     save('example_per.mat')
 else

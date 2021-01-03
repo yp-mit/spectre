4d3761b0af9d8c800b2ad7be7b090ae4f4774279
erseghe <erseghe@dei.unipd.it>
2021-01-04 00:45:40 +0100

    Add files via upload

--- bi-awgn/example_per.m
+++ bi-awgn/example_per.m
@@ -1,18 +1,21 @@
 % PACKET ERROR RATE BOUNDS - LOWER BOUNDS ON ERROR RATE
 
-if(1) % set to (1) if you wish to run the converse_mc function
+if(0) % set to (1) if you wish to run the converse_mc function
 
     n = 1e3;
     R = 1/2;
     Pev = 10.^([-0.5:-0.25:-1.75,-2:-0.5:-7]);
     uno = ones(size(Pev));
 
-    % normal approximation - expected execution time 4 sec
+    % expected execution times evaluated on a MacBook Pro
+    % normal approximation - expected execution time 2 sec
     tic; SNRdBNA = converse_mc(n*uno,Pev,R*uno,'normal','error'); toc
-    % O(n^-2) approximation - expected execution time 40 sec
-    tic; SNRdBPPV = converse_mc(n*uno,Pev,R*uno,'On2','error'); toc
-    % O(n^-3) approximation - expected execution time 80 sec
-    tic; SNRdBPPVb = converse_mc(n*uno,Pev,R*uno,'On3','error'); toc
+    % O(n^-2) approximation - expected execution time 20 sec
+    tic; SNRdBPPV2 = converse_mc(n*uno,Pev,R*uno,'On2','error'); toc
+    % O(n^-3) approximation - expected execution time 50 sec
+    tic; SNRdBPPV3 = converse_mc(n*uno,Pev,R*uno,'On3','error'); toc
+    % full approximation - expected execution time 40 min
+    tic; SNRdBPPV = converse_mc(n*uno,Pev,R*uno,'full','error'); toc
 
     save('example_per.mat')
 else
@@ -23,11 +26,11 @@ end
 close all
 figure(1)
 set(0,'defaulttextinterpreter','latex')
-semilogy(SNRdBPPV,Pev,'-',SNRdBPPVb,Pev,'--',SNRdBNA,Pev,'-.')
+semilogy(SNRdBPPV3,Pev,'-.',SNRdBPPV,Pev,'-',SNRdBPPV2,Pev,'--',SNRdBNA,Pev,'-.x')
 xlabel('SNR $E_b/N_0$')
-ylabel('error probability $P_e$')
+ylabel('packet error probability $P_e$')
 title(['n = ' num2str(n) ', R = ', num2str(R)])
 legend({},'interpreter','latex')
-legend('$O(n^{-2}\,)$ approx $\quad$','$O(n^{-3}\,)$ approx',...
+legend('$O(n^{-3}\,)$ approx','full approx','$O(n^{-2}\,)$ approx $\quad$',...
     'normal approx','Location','Best')
 grid
--- bi-awgn/example_speff.m
+++ bi-awgn/example_speff.m
@@ -1,21 +1,24 @@
 % SPECTRAL EFFICIENCY CURVES - UPPER BOUNDS ON RATE
 
-if(1) % set to (1) if you wish to run the converse_mc function
+if(0) % set to (1) if you wish to run the converse_mc function
 
     n = 1e3;
     Pe = 1e-5;
-    SNRdB = -15:10;
+    SNRdB = -15:7;
     uno = ones(size(SNRdB));tic
 
     % normal approximation - expected execution time 0.5 sec
     tic; rhoNA = 2*converse_mc(n*uno,Pe*uno,SNRdB,'normal'); % spectral efficiency 
     EbN0NA = SNRdB-10*log10(rhoNA); toc
-    % O(n^-2) approximation - expected execution time 20 sec
-    tic; rhoPPV = 2*converse_mc(n*uno,Pe*uno,SNRdB,'On2'); % spectral efficiency 
+    % O(n^-2) approximation - expected execution time 10 sec
+    tic; rhoPPV2 = 2*converse_mc(n*uno,Pe*uno,SNRdB,'On2'); % spectral efficiency 
+    EbN0PPV2 = SNRdB-10*log10(rhoPPV2); toc
+    % O(n^-3) approximation - expected execution time 20 sec
+    tic; rhoPPV3 = 2*converse_mc(n*uno,Pe*uno,SNRdB,'On3'); % spectral efficiency 
+    EbN0PPV3 = SNRdB-10*log10(rhoPPV3); toc
+    % full approximation - expected execution time 10 min
+    tic; rhoPPV = 2*converse_mc(n*uno,Pe*uno,SNRdB,'full'); % spectral efficiency 
     EbN0PPV = SNRdB-10*log10(rhoPPV); toc
-    % O(n^-3) approximation - expected execution time 40 sec
-    tic; rhoPPVb = 2*converse_mc(n*uno,Pe*uno,SNRdB,'On3'); % spectral efficiency 
-    EbN0PPVb = SNRdB-10*log10(rhoPPVb); toc
 
     save('example_speff.mat')
 else
@@ -26,11 +29,11 @@ end
 close all
 figure(1)
 set(0,'defaulttextinterpreter','latex')
-semilogy(EbN0PPV,rhoPPV,'-',EbN0PPVb,rhoPPVb,'--',EbN0NA,rhoNA,'-.')
+semilogy(EbN0PPV3,rhoPPV3,'--', EbN0PPV,rhoPPV,'-',EbN0PPV2,rhoPPV2,'--',EbN0NA,rhoNA,'-.x')
 xlabel('SNR $E_b/N_0$')
 ylabel('spectral efficiency $\rho$ [bit/s/Hz]')
 title(['n = ' num2str(n) ', Pe = ', num2str(Pe)])
 legend({},'interpreter','latex')
-legend('$O(n^{-2}\,)$ approx $\quad$','$O(n^{-3}\,)$ approx',...
+legend('$O(n^{-3}\,)$ approx $\quad$','full approx','$O(n^{-2}\,)$ approx',...
     'normal approx','Location','Best')
 grid
